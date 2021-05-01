64fc9ef7990449e3b9f551a8b34d3440d320313d
janm31415 <janm31415@gmail.com>
2021-05-01 20:20:30 +0200

    ml

--- MarsLander/cgalgo.cpp
+++ MarsLander/cgalgo.cpp
@@ -4,176 +4,149 @@
 
 #include "cgalgo.h"
 
+std::vector<vec2<int>> surface_points;
+std::vector<int> heights;
+RKISS rkiss;
+int landing_zone_x0, landing_zone_x1;
+simulation_data simdata;
+
 std::vector<int> compute_terrain_heights(const std::vector<vec2<int>>& pts) {
-    std::vector<int> heights;
-    heights.reserve(W);
-    auto it = pts.begin();
-    auto it_next = it;
-    ++it_next;
-    for (int x = 0; x < W; ++x) {
-        if (x > it_next->x) {
-            ++it;
-            ++it_next;
-        }
-
-        float alpha = (float)(x - it->x)/(float)(it_next->x - it->x);
-        float y_value = (1.f-alpha)*it->y + alpha*it_next->y;
-        heights.push_back((int)std::ceil(y_value));
+  std::vector<int> heights;
+  heights.reserve(W);
+  auto it = pts.begin();
+  auto it_next = it;
+  ++it_next;
+  for (int x = 0; x < W; ++x) {
+    if (x > it_next->x) {
+      ++it;
+      ++it_next;
     }
-
-    return heights;
+    
+    float alpha = (float)(x - it->x)/(float)(it_next->x - it->x);
+    float y_value = (1.f-alpha)*it->y + alpha*it_next->y;
+    heights.push_back((int)std::ceil(y_value));
+  }
+  
+  return heights;
 }
 
 void find_landingzone(int& x0, int& x1, const std::vector<int>& heights) {
-    x0 = x1 = 0;
-    int startcandidate = 0;
-    for (int i = 1; i < heights.size(); ++i) {
-        if (heights[i] != heights[startcandidate]) {
-            int new_length = i-1-startcandidate;
-            int old_length = x1-x0;
-            if (new_length > old_length) {
-                x0 = startcandidate;
-                x1 = i-1;
-            }
-            startcandidate = i;
-        }
+  x0 = x1 = 0;
+  int startcandidate = 0;
+  for (int i = 1; i < heights.size(); ++i) {
+    if (heights[i] != heights[startcandidate]) {
+      int new_length = i-1-startcandidate;
+      int old_length = x1-x0;
+      if (new_length > old_length) {
+        x0 = startcandidate;
+        x1 = i-1;
+      }
+      startcandidate = i;
     }
+  }
 }
 
 //#define OUTOFBOUNDSCLAMPING
 
 int clamp_angle(int angle, int R) {
-    if (angle>R+15)
-        angle = R+15;
-    else if (angle < R-15)
-        angle = R-15;
+  if (angle>R+15)
+    angle = R+15;
+  else if (angle < R-15)
+    angle = R-15;
 #if defined(OUTOFBOUNDSCLAMPING)
-    if (angle>90)
-        angle = 90;
-    if (angle<-90)
-        angle = -90;
+  if (angle>90)
+    angle = 90;
+  if (angle<-90)
+    angle = -90;
 #endif
-    return angle;
+  return angle;
 }
 
 int clamp_thrust(int thrust, int P) {
-    if (thrust > P+1)
-        thrust = P+1;
-    if (thrust < P-1)
-        thrust = P-1;
+  if (thrust > P+1)
+    thrust = P+1;
+  if (thrust < P-1)
+    thrust = P-1;
 #if defined(OUTOFBOUNDSCLAMPING)
-    if (thrust < 0)
-        thrust = 0;
-    if (thrust > 4)
-        thrust = 4;
+  if (thrust < 0)
+    thrust = 0;
+  if (thrust > 4)
+    thrust = 4;
 #endif
-    return thrust;
+  return thrust;
 }
 
-std::vector<vec2<int>> surface_points;
+chromosome generate_random_chromosome(int init_R, int init_P) {
+  chromosome c;
+  c.reserve(chromosome_size);
+  int last_R = init_R;
+  int last_P = init_P;
+  for (int i = 0; i < chromosome_size; ++i) {
+    int R = last_R + (int)(rkiss.rand64()%31)-15;
+    R = R<-90?-90:R>90?90:R;
+    int P = last_P + (int)(rkiss.rand64()%3)-1;
+    P = P<0?0:P>4?4:P;
+    gene g;
+    g.angle = R;
+    g.thrust = P;
+    last_R = R;
+    last_P = P;
+    c.push_back(g);
+  }
+  return c;
+}
 
-void simulate(sdata& sd, int angle, int thrust) {
-    const vec2<float> g(0, -3.711f);
-    angle = clamp_angle(angle, sd.R);
-    float ang = (float)angle*(float)pi/180.f;
-    vec2<float> f(std::cos(pi/2.f+ang), std::sin(pi/2.f+ang));
-    thrust = clamp_thrust(thrust, sd.P);
-    f = f*(float)thrust;
-    sd.P = thrust;
-    sd.R = angle;
-    sd.F -= thrust;
-    vec2<float> a = g+f;
-    sd.p = sd.p + sd.v + a*0.5f;
-    sd.v = sd.v + a;
+population generate_random_population(int init_R, int init_P) {
+  population p;
+  p.reserve(population_size);
+  for (int i = 0; i < population_size; ++i) {
+    p.emplace_back(generate_random_chromosome(init_R, init_P));
+  }
+  return p;
+}
+
+void simulate(simulation_data& sd, int angle, int thrust) {
+  const vec2<float> g(0, -3.711f);
+  angle = clamp_angle(angle, sd.R);
+  float ang = (float)angle*(float)pi/180.f;
+  vec2<float> f(std::cos(pi/2.f+ang), std::sin(pi/2.f+ang));
+  thrust = clamp_thrust(thrust, sd.P);
+  f = f*(float)thrust;
+  sd.P = thrust;
+  sd.R = angle;
+  sd.F -= thrust;
+  vec2<float> a = g+f;
+  sd.p = sd.p + sd.v + a*0.5f;
+  sd.v = sd.v + a;
 }
 
 void read_input(std::stringstream& strcin, std::stringstream& strerr) {
-    surface_points.clear();
-    int N; // the number of points used to draw the surface of Mars.
-    strcin >> N; strcin.ignore();
-    strerr << N << std::endl;
-    for (int i = 0; i < N; i++) {
-        int landX; // X coordinate of a surface point. (0 to 6999)
-        int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-        strcin >> landX >> landY; strcin.ignore();
-        strerr << landX << " " << landY << std::endl;
-        surface_points.emplace_back(landX, landY);
-    }
-}
-
-void do_main(std::stringstream& strcin, std::stringstream& strerr)
-{
-    read_input(strcin, strerr);
-    
-    auto heights = compute_terrain_heights(surface_points);
-    int land_x0, land_x1;
-    find_landingzone(land_x0, land_x1, heights);
-    strerr << "Landing zone: (" << land_x0 << ", " << land_x1 << ")" << std::endl;
-
-    float g = 3.711f;
-    float max_speed = 4.f;
-
-    int max_angle = (int)std::floor(acos(g/max_speed)*180.f/pi);
-    strerr << "Max angle: " << max_angle << std::endl;
-
-    float max_vs = 40.f;
-    float max_hs = 20.f;
-
-
-    int angle=-20;
-    int thrust=0;
-    bool move_right = false;
-
-
-    bool first_time = true;
-
-
-    sdata sda;
-    // game loop
-    while (1) {
-
-        angle=rand()%180-90;
-        thrust=rand()%5;
-
-        int X;
-        int Y;
-        int HS; // the horizontal speed (in m/s), can be negative.
-        int VS; // the vertical speed (in m/s), can be negative.
-        int F; // the quantity of remaining fuel in liters.
-        int R; // the rotation angle in degrees (-90 to 90).
-        int P; // the thrust power (0 to 4).
-        strcin >> X >> Y >> HS >> VS >> F >> R >> P; strcin.ignore();
-
-        if (!first_time) {
-
-            int PX = (int)std::round(sda.p.x);
-            int PY = (int)std::round(sda.p.y);
-            int VX = (int)std::round(sda.v.x);
-            int VY = (int)std::round(sda.v.y);
-            if (PX != X || PY != Y || VX != HS || VY != VS || sda.F != F || sda.R != R || sda.P != P) {
-                strerr << "X: " << sda.p.x << " vs " << X << std::endl;
-                strerr << "Y: " << sda.p.y << " vs " << Y << std::endl;
-                strerr << "HS: " << sda.v.x << " vs " << HS << std::endl;
-                strerr << "VS: " << sda.v.y << " vs " << VS << std::endl;
-                strerr << "F: " << sda.F << " vs " << F << std::endl;
-                strerr << "R: " << sda.R << " vs " << R << std::endl;
-                strerr << "P: " << sda.P << " vs " << P << std::endl;
-            }
-        } else {
-            strerr << X << " " << Y << " " << HS << " " << VS << " " << F << " " << R << " " << P << std::endl;
-            sda.F = F;
-            sda.P = P;
-            sda.R = R;
-            sda.p.x = X;
-            sda.p.y = Y;
-            sda.v.x = HS;
-            sda.v.y = VS;
-        }
-
-        simulate(sda, angle, thrust);
-
-
-        std::cout << angle << " " << thrust << std::endl;
-        first_time = false;
-    }
+  surface_points.clear();
+  int N; // the number of points used to draw the surface of Mars.
+  strcin >> N; strcin.ignore();
+  strerr << N << std::endl;
+  for (int i = 0; i < N; i++) {
+    int landX; // X coordinate of a surface point. (0 to 6999)
+    int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+    strcin >> landX >> landY; strcin.ignore();
+    strerr << landX << " " << landY << std::endl;
+    surface_points.emplace_back(landX, landY);
+  }
+  heights = compute_terrain_heights(surface_points);
+  find_landingzone(landing_zone_x0, landing_zone_x1, heights);
+  
+  int X;
+  int Y;
+  int HS; // the horizontal speed (in m/s), can be negative.
+  int VS; // the vertical speed (in m/s), can be negative.
+  int F; // the quantity of remaining fuel in liters.
+  int R; // the rotation angle in degrees (-90 to 90).
+  int P; // the thrust power (0 to 4).
+  strcin >> X >> Y >> HS >> VS >> F >> R >> P;
+  
+  simdata.p = vec2<float>(X,Y);
+  simdata.v = vec2<float>(HS,VS);
+  simdata.F = F;
+  simdata.R = R;
+  simdata.P = P;
 }
--- MarsLander/cgalgo.h
+++ MarsLander/cgalgo.h
@@ -12,25 +12,53 @@
 #define W 7000
 #define H 3000
 
+// random number generator
+class RKISS {
+  
+  uint64_t a, b, c, d;
+  
+  uint64_t rotate_L(uint64_t x, unsigned k) const {
+    return (x << k) | (x >> (64 - k));
+  }
+  
+public:
+  RKISS(int seed = 73) {
+    a = 0xF1EA5EED, b = c = d = 0xD4E12C77;
+    for (int i = 0; i < seed; ++i) // Scramble a few rounds
+    rand64();
+  }
+  
+  uint64_t rand64() {
+    const uint64_t e = a - rotate_L(b,  7);
+    a = b ^ rotate_L(c, 13);
+    b = c + rotate_L(d, 37);
+    c = d + e;
+    return d = e + a;
+  }
+  
+  template<typename T> T rand() { return T(rand64()); }
+};
+
+
 template <typename T>
 struct vec2
 {
-T x, y;
-
-typedef T value_type;
-
-vec2();
-
-vec2(const T& t);
-
-vec2(const T& _x, const T& _y);
-
-template <typename T2>
-T& operator [] (T2 i);
-
-template <typename T2>
-const T& operator [] (T2 i) const;
-
+  T x, y;
+  
+  typedef T value_type;
+  
+  vec2();
+  
+  vec2(const T& t);
+  
+  vec2(const T& _x, const T& _y);
+  
+  template <typename T2>
+  T& operator [] (T2 i);
+  
+  template <typename T2>
+  const T& operator [] (T2 i) const;
+  
 };
 
 template <typename T>
@@ -53,193 +81,215 @@ const T& vec2<T>::operator [] (T2 i) const { return (&x)[i]; }
 template <typename T>
 inline vec2<T> operator + (const vec2<T>& a)
 {
-return vec2<T>(+a.x, +a.y);
+  return vec2<T>(+a.x, +a.y);
 }
 
 template <typename T>
 inline vec2<T> operator - (const vec2<T>& a)
 {
-return vec2<T>(-a.x, -a.y);
+  return vec2<T>(-a.x, -a.y);
 }
 
 template <typename T>
 inline vec2<T> operator + (const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(left.x + right.x, left.y + right.y);
+  return vec2<T>(left.x + right.x, left.y + right.y);
 }
 
 template <typename T>
 inline vec2<T> operator - (const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(left.x - right.x, left.y - right.y);
+  return vec2<T>(left.x - right.x, left.y - right.y);
 }
 
 template <typename T>
 inline vec2<T> operator * (const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(left.x * right.x, left.y * right.y);
+  return vec2<T>(left.x * right.x, left.y * right.y);
 }
 
 template <typename T>
 inline vec2<T> operator * (const T& s, const vec2<T>& right)
 {
-return vec2<T>(s * right.x, s * right.y);
+  return vec2<T>(s * right.x, s * right.y);
 }
 
 template <typename T>
 inline vec2<T> operator * (const vec2<T>& left, const T& s)
 {
-return vec2<T>(left.x * s, left.y * s);
+  return vec2<T>(left.x * s, left.y * s);
 }
 
 template <typename T>
 inline vec2<T> operator / (const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(left.x / right.x, left.y / right.y);
+  return vec2<T>(left.x / right.x, left.y / right.y);
 }
 
 template <typename T>
 inline vec2<T> operator / (const T& s, const vec2<T>& right)
 {
-return vec2<T>(s / right.x, s / right.y);
+  return vec2<T>(s / right.x, s / right.y);
 }
 
 template <typename T>
 inline vec2<T> operator / (const vec2<T>& left, const T& s)
 {
-return vec2<T>(left.x / s, left.y / s);
+  return vec2<T>(left.x / s, left.y / s);
 }
 
 template <typename T>
 inline vec2<T> min(const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(min(left.x, right.x), min(left.y, right.y));
+  return vec2<T>(min(left.x, right.x), min(left.y, right.y));
 }
 
 template <typename T>
 inline vec2<T> max(const vec2<T>& left, const vec2<T>& right)
 {
-return vec2<T>(max(left.x, right.x), max(left.y, right.y));
+  return vec2<T>(max(left.x, right.x), max(left.y, right.y));
 }
 
 template <typename T>
 inline vec2<T> abs(const vec2<T>& a)
 {
-return vec2<T>(abs(a.x), abs(a.y));
+  return vec2<T>(abs(a.x), abs(a.y));
 }
 
 template <>
 inline vec2<float> abs(const vec2<float>& a)
 {
-return vec2<float>(fabs(a.x), fabs(a.y));
+  return vec2<float>(fabs(a.x), fabs(a.y));
 }
 
 template <typename T>
 inline vec2<T> sqrt(const vec2<T>& a)
 {
-return vec2<T>(sqrt(a.x), sqrt(a.y));
+  return vec2<T>(sqrt(a.x), sqrt(a.y));
 }
 
 template <typename T>
 inline vec2<T> rsqrt(const vec2<T>& a)
 {
-return vec2<T>(rsqrt(a.x), rsqrt(a.y));
+  return vec2<T>(rsqrt(a.x), rsqrt(a.y));
 }
 
 template <typename T>
 inline vec2<T> reciprocal(const vec2<T>& a)
 {
-return vec2<T>(reciprocal(a.x), reciprocal(a.y));
+  return vec2<T>(reciprocal(a.x), reciprocal(a.y));
 }
 
 template <typename T>
 inline T dot(const vec2<T>& left, const vec2<T>& right)
 {
-return left.x*right.x + left.y*right.y;
+  return left.x*right.x + left.y*right.y;
 }
 
 template <typename T>
 inline T length(const vec2<T>& a)
 {
-return sqrt(dot(a, a));
+  return sqrt(dot(a, a));
 }
 
 template <>
 inline float length(const vec2<float>& a)
 {
-return std::sqrt(dot(a, a));
+  return std::sqrt(dot(a, a));
 }
 
 template <>
 inline double length(const vec2<double>& a)
 {
-return std::sqrt(dot(a, a));
+  return std::sqrt(dot(a, a));
 }
 
 template <typename T>
 inline T distance(const vec2<T>& left, const vec2<T>& right)
 {
-return length(left - right);
+  return length(left - right);
 }
 
 template <typename T>
 inline T distance_sqr(const vec2<T>& left, const vec2<T>& right)
 {
-auto a = (left - right);
-return dot(a, a);
+  auto a = (left - right);
+  return dot(a, a);
 }
 
 template <typename T>
 inline vec2<T> normalize(const vec2<T>& a)
 {
-return a * rsqrt(dot(a, a));
+  return a * rsqrt(dot(a, a));
 }
 
 template <>
 inline vec2<float> normalize(const vec2<float>& a)
 {
-return a / std::sqrt(dot(a, a));
+  return a / std::sqrt(dot(a, a));
 }
 
 template <>
 inline vec2<double> normalize(const vec2<double>& a)
 {
-return a / std::sqrt(dot(a, a));
+  return a / std::sqrt(dot(a, a));
 }
 
 template <typename T>
 inline bool operator < (const vec2<T>& left, const vec2<T>& right)
 {
-return (left[0] == right[0]) ? (left[1] < right[1]) : (left[0] < right[0]);
+  return (left[0] == right[0]) ? (left[1] < right[1]) : (left[0] < right[0]);
 }
 
 template <typename T>
 inline bool operator > (const vec2<T>& left, const vec2<T>& right)
 {
-return (left[0] == right[0]) ? (left[1] > right[1]) : (left[0] > right[0]);
+  return (left[0] == right[0]) ? (left[1] > right[1]) : (left[0] > right[0]);
 }
 
 template <typename T>
 inline bool operator == (const vec2<T>& left, const vec2<T>& right)
 {
-return (left[0] == right[0]) && (left[1] == right[1]);
+  return (left[0] == right[0]) && (left[1] == right[1]);
 }
 
 template <typename T>
 inline bool operator != (const vec2<T>& left, const vec2<T>& right)
 {
-return !(left == right);
+  return !(left == right);
 }
 
-struct sdata {
-    vec2<float> p,v;
-    int F; // the quantity of remaining fuel in liters.
-    int R; // the rotation angle in degrees (-90 to 90).
-    int P; // the thrust power (0 to 4).
+struct simulation_data {
+  vec2<float> p,v;
+  int F; // the quantity of remaining fuel in liters.
+  int R; // the rotation angle in degrees (-90 to 90).
+  int P; // the thrust power (0 to 4).
 };
 
+struct gene {
+  int angle;
+  int thrust;
+};
+
+typedef std::vector<gene> chromosome;
+typedef std::vector<chromosome> population;
+
+#define chromosome_size 60
+#define population_size 100
 
 extern std::vector<vec2<int>> surface_points;
+extern std::vector<int> heights;
+extern int landing_zone_x0;
+extern int landing_zone_x1;
+extern simulation_data simdata;
 
+chromosome generate_random_chromosome(int init_R, int init_P);
+population generate_random_population(int init_R, int init_P);
+
+/*
+ This method fills surface_points with the terrain,
+ generates heights, which for each pixel x provides the height h,
+ generates the landing zone interval [landing_zone_x0, landing_zone_x1],
+ and fills simdata with the input data for the mars lander.
+ */
 void read_input(std::stringstream& strcin, std::stringstream& strerr);
-void do_main(std::stringstream& strcin, std::stringstream& strerr);
